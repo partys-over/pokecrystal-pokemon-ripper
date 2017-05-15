2a3b293fc778a63722819606fdee83fd182da54b
dominic <radic4lly@gmail.com>
2017-05-14 18:37:58 -0700

    python > perl

---
+++ script.py
@@ -0,0 +1,4 @@
+data = open('data.csv', 'r')
+output = open('output.txt', 'r+')
+
+replacepokemon = raw_input('What pokemon are you replacing, and what pokemon do you want to replace it with? Seperate your answers with a comman and a space \', \'\n')
